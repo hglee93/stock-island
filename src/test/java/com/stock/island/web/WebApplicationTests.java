4cf0faf7ed1e8b5acbd1bdb5788f0b746cccd42a
hglee93 <jeuslhg@gmail.com>
2021-08-11 22:32:55 +0900

    project init

---
+++ src/test/java/com/stock/island/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.island.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
